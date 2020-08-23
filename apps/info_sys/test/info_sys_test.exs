798246224d92f9fbee3cfd64170987d7c4be0caf
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-08-23 17:06:40 -0400

    Test InfoSys.compute/2 with a backend stub

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -1,4 +1,51 @@
 defmodule InfoSysTest do
   use ExUnit.Case
-  doctest InfoSys
+  alias InfoSys.Result
+
+  # A stub that acts like our Wolfram backend.
+  defmodule TestBackend do
+    def name() do
+      "Wolfram"
+    end
+
+    def compute("result", _opts) do
+      [%Result{backend: __MODULE__, text: "result"}]
+    end
+
+    def compute("none", _opts) do
+      []
+    end
+
+    # Sleeps forever.
+    def compute("timeout", _opts) do
+      Process.sleep(:infinity)
+    end
+
+    # Raises error.
+    def compute("boom", _opts) do
+      raise "boom!"
+    end
+  end
+
+  test "compute/2 with sbackend results" do
+    assert [%Result{backend: TestBackend, text: "result"}] =
+             InfoSys.compute("result", backends: [TestBackend])
+  end
+
+  test "compute/2 with no backend results" do
+    assert [] = InfoSys.compute("none", backends: [TestBackend])
+  end
+
+  test "compute/2 with timeout returns no results" do
+    # Shorten the timeout inverval so that our test is fast.
+    results = InfoSys.compute("timeout", backends: [TestBackend], timeout: 10)
+    assert results == []
+  end
+
+  # Capture the log in order to keep our test from printing out error messages
+  # when the exeption fires.
+  @tag :capture_log
+  test "compute/2 discards backend errors" do
+    assert InfoSys.compute("boom", backends: [TestBackend]) == []
+  end
 end
